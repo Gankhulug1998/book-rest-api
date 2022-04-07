b3a8c00ab7da242ec1489379e61e81a05150fce2
gankhulug <gankhulug@gerege.mn>
2022-04-07 14:41:48 +0800

    test

---
+++ test.js
@@ -0,0 +1,9 @@
+const https = require('https');
+const fs = require('fs');
+const certPath = '/etc/letsencrypt/live/httpstest.codekoala.net';
+https.createServer({
+    key: fs.readFileSync(`${certPath}/privkey.pem`),
+    cert: fs.readFileSync(`${certPath}/fullchain.pem`)
+}, (req, res) => {
+    res.end('<h1>Hello!</h1>')
+}).listen(443);
